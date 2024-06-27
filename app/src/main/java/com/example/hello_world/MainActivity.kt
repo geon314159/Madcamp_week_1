9c48d1f92bb5ab999d2078a6c391537016357d89
geon314159 <geon314159@kaist.ac.kr>
2024-06-27 17:21:06 +0900

    test5

--- app/src/main/java/com/example/hello_world/MainActivity.kt
+++ app/src/main/java/com/example/hello_world/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             Hello_worldTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Androidd222222222dd",
+                        name = "Androidd2224123523523222222dd",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
